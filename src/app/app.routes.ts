64730fc8a0229c62bca961fd2b4699f6d07a0317
Marfalling <dame.tufuerza.pegazo988@gmail.com>
2024-09-13 15:34:55 -0500

    Creando estructura del proyecto y agregando la pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./Components/layout/layout.component'),
+        children: [ 
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./Pages/dashboard/dashboard.component')
+            },
+
+            {
+                path: 'colaboradores',
+                loadComponent: () => import('./Pages/colaboradores/colaboradores.component')
+            },
+
+            {
+                path: 'eventos',
+                loadComponent: () => import('./Pages/eventos/eventos.component')
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
