a7218e94b2147e1a5bf10873bda9c53cdc9b430f
Marfalling <dame.tufuerza.pegazo988@gmail.com>
2024-09-16 00:52:38 -0500

    se agrega barra de filtro y de search y arregla vista de colaboradores

--- src/app/Pages/colaboradores/colaboradores.component.spec.ts
+++ src/app/Pages/colaboradores/colaboradores.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ColaboradoresComponent } from './colaboradores.component';
+import  ColaboradoresComponent  from './colaboradores.component';
 
 describe('ColaboradoresComponent', () => {
   let component: ColaboradoresComponent;
