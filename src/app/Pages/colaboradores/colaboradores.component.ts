9d5f96297f8847c7f07e442ea513ce9908c50932
Marfalling <dame.tufuerza.pegazo988@gmail.com>
2024-09-23 23:24:46 -0500

    modal de agregar colaborador

--- src/app/Pages/colaboradores/colaboradores.component.ts
+++ src/app/Pages/colaboradores/colaboradores.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-colaboradores',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, FormsModule], // No necesitas importar tu propio componente aquí.
   templateUrl: './colaboradores.component.html',
-  styleUrl: './colaboradores.component.css'
+  styleUrls: ['./colaboradores.component.css'] 
 })
 export default class ColaboradoresComponent {
+  showModal: boolean = false;
+
+  AgregarColab() {
+    this.showModal = true;
+  }
+
+  CerrarColab() {
+    this.showModal = false;
+  }
+
 
 }
